761f5a523ff858d3b06c942ac43a43078fc2d29b
Amine <Amine@Amines-MacBook-Pro.local>
2018-02-19 00:10:49 +0100

    Initial commit

---
+++ src/organisationevenements/Accueil.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package organisationevenements;
+
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Amine
+ */
+public class Accueil extends JFrame {
+
+    //Declaration panels
+    private JFrame frame;
+    private JPanel pPrincipal;
+    private JPanel pAcces;
+    private JScrollPane pConsultation;
+    //Declaration Tableau
+    private JTable tConsultation;
+    //Declaration objets acces
+    private JComboBox cbUtilisateur;
+    private JButton bAcces;
+    private String[] lUtilisateurs = {"Organnisme", "Organisateur", "Abonné"};
+    
+
+    public Accueil() {
+        this.design();
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public void design() {
+        this.setTitle("Accueil");
+        this.setBounds(new Rectangle(0, 0, 900, 500));
+        this.setLayout(new BorderLayout());
+        //Panel principal
+        pPrincipal = new JPanel();
+        pPrincipal.setLayout(new BorderLayout());
+        this.add(pPrincipal, BorderLayout.CENTER);
+        //panel Accès
+        pAcces = new JPanel();
+        pAcces.setBorder(BorderFactory.createTitledBorder("Accès"));
+        pPrincipal.add(pAcces, BorderLayout.NORTH);
+        cbUtilisateur = new JComboBox(lUtilisateurs);
+        bAcces = new JButton("Accéder");
+        pAcces.add(cbUtilisateur);
+        pAcces.add(bAcces);
+        //Panel Consultation
+        pConsultation= new JScrollPane();
+        pConsultation.setBorder(BorderFactory.createTitledBorder("Liste d'événements"));
+        pPrincipal.add(pConsultation, BorderLayout.CENTER);
+        //Table consultation
+        tConsultation=new JTable();
+        
+    }
+
+    public JTable gettConsultation() {
+        return tConsultation;
+    }
+
+    public JComboBox getCbUtilisateur() {
+        return cbUtilisateur;
+    }
+
+    public JButton getbAcces() {
+        return bAcces;
+    }
+
+    
+    
+}
