cc66bc75bcd3424c6bf5790aa2d0218a779efd02
AnaXaX <anas.boudyach@gmail.com>
2018-03-02 13:10:58 +0100

    nkoune

--- src/OrganisationEvenements/controller/OrganisationEvenements.java
+++ src/OrganisationEvenements/controller/OrganisationEvenements.java
@@ -4,21 +4,21 @@ import OrganisationEvenements.controller.Lists;
 import OrganisationEvenements.view.Accueil;
 
 public class OrganisationEvenements {
+
 	private static Lists lists;
-        private static Accueil fenetreAccueil;
-	
+	private static Accueil fenetreAccueil;
+
 	public static void main(String[] args) {
 		lists = new Lists();
 		fenetreAccueil = new Accueil();
 	}
-	
+
 	public static Lists getLists() {
 		return lists;
 	}
 
-    public static Accueil getFenetreAccueil() {
-        return fenetreAccueil;
-    }
-        
-        
+	public static Accueil getFenetreAccueil() {
+		return fenetreAccueil;
+	}
+
 }
